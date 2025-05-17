0bafe6cd48e4b4114acefda47ad87f32efef9aaf
Vinnieotieno <121629812+Vinnieotieno@users.noreply.github.com>
2025-05-18 02:10:08 +0300

    Update 1-batch_processing.py

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,43 +1,44 @@
-#!/usr/bin/python3
-import mysql.connector
+
+"""
+- Write a function stream_users_in_batches(batch_size) that fetches rows in batches
+
+- Write a function batch_processing() that processes each batch to filter users over the age of25`
+
+- You must use no more than 3 loops in your code. Your script must use the yield generator
+"""
+
+import pymysql
+import pymysql.cursors
+
 
 def stream_users_in_batches(batch_size):
-    """
-    Generator that fetches users in batches of `batch_size`.
-    Yields one batch (a list of rows) at a time.
-    """
+    connection = pymysql.connect(
+        host="localhost",
+        user="bix",
+        password="",
+        database="ALX_prodev",
+        cursorclass=pymysql.cursors.DictCursor,
+    )
+
+    rows = None
+
     try:
-        connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="12345",  
-            database="ALX_prodev"
-        )
-        cursor = connection.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM user_data")
-
-        while True:
-            batch = cursor.fetchmany(batch_size)
-            if not batch:
-                break
-            yield batch
-
-    except mysql.connector.Error as err:
-        print(f"Database error: {err}")
-
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM user_data LIMIT %s", batch_size)
+            rows = cursor.fetchall()
+            if not rows:
+                return None
+            yield rows
     finally:
-        if cursor:
-            cursor.close()
-        if connection:
-            connection.close()
+        connection.close()
 
 
 def batch_processing(batch_size):
-    """
-    Processes each batch to filter users over the age of 25.
-    Prints each matching user.
-    """
-    for batch in stream_users_in_batches(batch_size):  # 1st loop
-        for user in batch:  # 2nd loop
-            if user['age'] > 25:
-                print(user)
+    for batch in stream_users_in_batches(batch_size):
+        users_over_25 = (user for user in batch if user["age"] > 25)
+        for user in users_over_25:
+            yield user
+            print(user)
+
+
+batch_processing(50)
