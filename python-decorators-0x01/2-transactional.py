dc9759ca646ce1f111d7bace20be487332cb23f1
Vinnieotieno <121629812+Vinnieotieno@users.noreply.github.com>
2025-05-22 15:31:24 +0300

    Create 2-transactional.py

---
+++ python-decorators-0x01/2-transactional.py
@@ -0,0 +1,20 @@
+def transactional(func):
+    @functools.wraps(func)
+    def wrapper(conn, *args, **kwargs):
+        try:
+            result = func(conn, *args, **kwargs)
+            conn.commit()
+            return result
+        except Exception as e:
+            conn.rollback()
+            print(f"[ERROR] Transaction rolled back: {e}")
+            raise
+    return wrapper
+
+@with_db_connection
+@transactional
+def update_user_email(conn, user_id, new_email):
+    cursor = conn.cursor()
+    cursor.execute("UPDATE users SET email = ? WHERE id = ?", (new_email, user_id))
+
+update_user_email(user_id=1, new_email='vincent@gmail.com')
