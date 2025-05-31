6839bdddb1639db35240413ac6c2289725fa8cce
Vinnieotieno <121629812+Vinnieotieno@users.noreply.github.com>
2025-05-31 15:26:41 +0300

    Update urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+
+from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
 
-router = DefaultRouter()
-router.register('conversations', ConversationViewSet)
-router.register('messages', MessageViewSet)
+router = routers.DefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversations')
+router.register(r'messages', MessageViewSet, basename='messages')
 
 urlpatterns = [
     path('', include(router.urls)),
